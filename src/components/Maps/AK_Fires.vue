095959aec1b39b2ae9485bcf72df6f37ff2e1c93
Bob Torgerson <rltorgerson@alaska.edu>
2018-05-14 11:55:11 -0800

    Changed the text of the lightning strikes to show 48 hours rather than 2 weeks.

--- src/components/Maps/AK_Fires.vue
+++ src/components/Maps/AK_Fires.vue
@@ -110,7 +110,7 @@ export default {
           first: fireLayerGroup,
           second: secondFireLayerGroup
         },
-        '2w_lightning': {
+        'lightning': {
           first: lightningLayerGroup,
           second: secondLightningLayerGroup
         },
@@ -333,11 +333,11 @@ export default {
           'abstract': '<img src="static/legend3.svg"/><p>This layer shows fires that occurred or are actively burning this year.</p><p>We update our map each hour from the source data available at the <a href="https://fire.ak.blm.gov" target="_blank" rel="externa">AICC</a> web site.</p><p><em>Where do most fires occur?  Where do most of the large fires occur?</em></p>'
         },
         {
-          'name': '2w_lightning',
-          'title': 'Lightning strikes, last 2 weeks',
+          'name': 'lightning',
+          'title': 'Lightning strikes, last 48 hours',
           'local': true,
           'legend': false,
-          'abstract': '<p>This layer shows all of the recorded lightning strikes that have occurred over the course of the past two weeks.</p><p>Many of the fires that occur during the summer in Alaska are caused by lightning strikes, thus seeing the recorded lightning strikes can be a good indication of potential fire starting points. See if you can find some lightning strikes that line up with recently started forest fires.</p>'
+          'abstract': '<p>This layer shows all of the recorded lightning strikes that have occurred over the course of the last 48 hours.</p><p>Many of the fires that occur during the summer in Alaska are caused by lightning strikes, thus seeing the recorded lightning strikes can be a good indication of potential fire starting points. See if you can find some lightning strikes that line up with recently started forest fires.</p>'
         },
         {
           'name': 'viirs',
