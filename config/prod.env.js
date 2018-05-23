2a39ce5cee02b5a313b288e09a6c0cab8458f7b2
Bruce Crevensten <bruce.crevensten@gmail.com>
2018-05-23 07:06:51 -0700

    update default prod wms url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  GEOSERVER_WMS_URL: '"http://54.202.138.90:8080/geoserver/wms"',
+  GEOSERVER_WMS_URL: '"http://54.70.10.93:8080/geoserver/wms/"',
   FIRE_FEATURES_URL: '"http://aicc-fire-api.openshift.snap.uaf.edu/"',
   FIRE_TIME_SERIES_URL: '"http://aicc-fire-api.openshift.snap.uaf.edu/fire-time-series"',
   LIGHTNING_FEATURES_URL: '"http://aicc-fire-api.openshift.snap.uaf.edu/lightning-data"',
