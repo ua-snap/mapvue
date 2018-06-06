343fb764bbad02e754a2cb755d230cf616734f8a
Bob Torgerson <rltorgerson@alaska.edu>
2018-06-05 18:17:03 -0800

    Additions to SNAP RCP6 map.

--- src/components/Maps/SNAP_RCP6.vue
+++ src/components/Maps/SNAP_RCP6.vue
@@ -49,22 +49,22 @@ export default {
       layers: [
         {
           'abstract': 'This layer shows the decadal average length of growing season in the 2090s, which refers to the number of days between the days of thaw and freeze.\n\nEstimated days of freeze and thaw are calculated by assuming a linear change in temperature between consecutive months.  When consecutive monthly midpoints have opposite sign temperatures, the day of transition (freeze or thaw) is the day between them on which temperature crosses zero degrees C.\n\n[Read more about this data set](http://ckan.snap.uaf.edu/dataset/projected-derived-dof-dot-logs-2km-cmip5-ar5), including the algorithms used to generate this data layer.',
-          'name': 'geonode:logs_5modelavg_rcp60_2090_2099_3572',
+          'name': 'snap_rcp:logs_2090_rcp6',
           'title': '2090s Length of Growing Season, RCP 6.0'
         },
         {
           'abstract': 'This layer shows the decadal average length of growing season in the 2010s, which refers to the number of days between the days of thaw and freeze.\n\nEstimated days of freeze and thaw are calculated by assuming a linear change in temperature between consecutive months.  When consecutive monthly midpoints have opposite sign temperatures, the day of transition (freeze or thaw) is the day between them on which temperature crosses zero degrees C.\n\n[Read more about this data set](http://ckan.snap.uaf.edu/dataset/projected-derived-dof-dot-logs-2km-cmip5-ar5), including the algorithms used to generate this data layer.',
-          'name': 'geonode:logs_5modelavg_rcp60_2010_2019_3857',
+          'name': 'snap_rcp:logs_2010_rcp6',
           'title': '2010s Length of Growing Season, RCP 6.0'
         },
         {
           'abstract': 'This layer shows projected, downscaled mean decadal temperature for 2090 using a five-model average. The spatial resolution of this dataset is 2x2km. The downscaling process for this layer utilizes PRISM climatological datasets from 1961-1990.\n\n[Download source data for this layer](http://ckan.snap.uaf.edu/dataset/projected-monthly-and-derived-temperature-products-2km-cmip5-ar5), and other related products.',
-          'name': 'geonode:tas_2090s_fixed_3572',
+          'name': 'snap_rcp:tas_2090_rcp6',
           'title': '2090s Mean Annual Temperature, RCP 6.0'
         },
         {
           'abstract': 'This layer shows projected, downscaled mean decadal temperature for 2010 using a five-model average.  The spatial resolution of this dataset is 2x2km.   The downscaling process for this layer utilizes PRISM climatological datasets from 1961-1990.\n\n[Download source data](http://ckan.snap.uaf.edu/dataset/projected-monthly-and-derived-temperature-products-2km-cmip5-ar5) for this layer, and other related products.',
-          'name': 'geonode:tas_decadal_mean_annual_mean_c_5modelavg_rcp60_2010_2019',
+          'name': 'snap_rcp:tas_2010_rcp6',
           'title': '2010s Mean Annual Temperature, RCP 6.0'
         },
         {
