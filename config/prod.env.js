8728a472e5cd8592aa0d63f871521727869e7464
Bruce Crevensten <bruce.crevensten@gmail.com>
2019-07-10 14:18:48 -0700

    Remove unused lightning URL endpoint from configuration

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,6 @@ module.exports = {
   GEOSERVER_WMS_URL: '"http://54.70.10.93:8080/geoserver/wms/"',
   FIRE_FEATURES_URL: '"http://mv-aicc-shim-production.us-west-2.elasticbeanstalk.com/"',
   FIRE_TIME_SERIES_URL: '"http://mv-aicc-shim-production.us-west-2.elasticbeanstalk.com/fire-time-series"',
-  LIGHTNING_FEATURES_URL: '"http://mv-aicc-shim-production.us-west-2.elasticbeanstalk.com/lightning-data"',
   VIIRS_URL: '"http://mv-aicc-shim-production.us-west-2.elasticbeanstalk.com/viirs"',
   MV_GOOGLE_ANALYTICS_TOKEN: "'" + process.env.MV_GOOGLE_ANALYTICS_TOKEN + "'"
 }
