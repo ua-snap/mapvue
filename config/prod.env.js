406868c135745480f84ad86dace687a784b10e2c
Bruce Crevensten <bruce.crevensten@gmail.com>
2020-05-01 10:49:49 -0700

    adjust production endpoint for fire data API

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   GEOSERVER_WMS_URL: '"http://52.24.100.6:8080/geoserver/wms"',
-  FIRE_FEATURES_URL: '"http://mv-aicc-shim-production.us-west-2.elasticbeanstalk.com/fires.geojson"',
-  VIIRS_URL: '"http://mv-aicc-shim-production.us-west-2.elasticbeanstalk.com/viirs.geojson"',
+  FIRE_FEATURES_URL: '"http://mv-aicc-fire-shim-production.us-west-2.elasticbeanstalk.com/fires.geojson"',
+  VIIRS_URL: '"http://mv-aicc-fire-shim-production.us-west-2.elasticbeanstalk.com/viirs.geojson"',
   MV_GOOGLE_ANALYTICS_TOKEN: "'" + process.env.MV_GOOGLE_ANALYTICS_TOKEN + "'"
 }
