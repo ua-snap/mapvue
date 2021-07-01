b7c59369157d218e83d84c64ddb49377da420994
Bruce Crevensten <bruce.crevensten@gmail.com>
2021-07-01 15:24:43 -0700

    Add default root path redirect to SNAP tools page

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/',
+      beforeEnter(to, from, next) {
+        window.location.href = 'https://uaf-snap.org/tools-overview/'
+      }
+    },
+    {
       path: '/map/fires',
       beforeEnter(to, from, next) {
         window.location.href = 'https://snap.uaf.edu/tools/alaska-wildfires'
